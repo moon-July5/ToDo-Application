664985c8cd0e94ae58b5b6a8bc8305783cdf8731
moon-July5 <rudtjr0705@naver.com>
2022-07-20 21:22:24 +0900

    LocalStorage

--- todo-react-app/src/service/ApiService.js
+++ todo-react-app/src/service/ApiService.js
@@ -2,15 +2,25 @@
     백엔드로 요청을 보낼 때 사용할 유틸리티 함수
 */
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+  let headers = new Headers({
+    "Content-Type" : "application/json"
+  });
+  
+  // 로컬 스토리지에서 ACCESS_TOKEN 가져오기
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if(accessToken && accessToken !== null){
+    headers.append("Authorization", "Bearer "+accessToken);
+  }
+
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
+
   if (request) {
     // GET method
     options.body = JSON.stringify(request);
@@ -37,7 +47,9 @@ export function signin(userDTO){
   return call("/auth/signin", "POST", userDTO)
   .then((response) => {
     if(response.token){
-      // token이 존재하는 경우 Todo 화면으로 redirect
+      // 로컬 스토리지에 토큰 저장
+      localStorage.setItem(ACCESS_TOKEN, response.token);
+      // token이 존재하는 경우 Todo 화면으로 리다이렉트
       window.location.href = "/";
     }
   });
